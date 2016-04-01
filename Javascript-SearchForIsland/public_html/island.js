bbedc9d2439ad085e41947bfd6bcf20443c94c20
raliclo@gmail.com <raliclo@gmail.com>
2016-04-01 11:16:06 -0700

    Updated

--- Javascript-SearchForIsland/public_html/island.js
+++ Javascript-SearchForIsland/public_html/island.js
@@ -58,6 +58,7 @@ var matrix3 = [
     [1, 0, 0, 0, 1]];
 
 var pop2Dmatrix = function (i, j) {
+    // ES2016 arrow operator.
     // Works well in chrome browser, yet it is unable to formatted corredctly by netbeans IDE8.1
 //    var matrix = new Array(i).fill(false).map(row => new Array(j).fill(false));
     return matrix;
